2fb175f8dffb0e78b91c4608d3d11093e16ea95a
zluevano <111832319+Zluevano@users.noreply.github.com>
2022-09-29 04:33:13 +0000

    HW3 was extremely easy assuming I did this correctly, the formula was checked on mathway.

--- .ccls-cache/@home@runner@CSCIHW6ZL/HW2@HW2.cpp
+++ .ccls-cache/@home@runner@CSCIHW6ZL/HW2@HW2.cpp
@@ -3,80 +3,81 @@
 #include <ctime>
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 
 ////////////////
-int takeInt(double x);
-double takeDouble(double x);
 void check (int num1, double num2, double num3);
+
+double AbsFunt (double x);
+
+double mult (double num4, double num5);
+
+int SqrIt (int num6);
 ////////////////
 int main()
 {
-char choice;
-double input;
-int User;
-bool ValidPick = false;
-double x;
+int num1;
+double num2, num3;
+double num4, num5;
+double num6, num7;
+cin >> num1 >> num2 >> num3;
+check (num1, num2, num3);
 
-cout << "Please enter an integer or a double number:" << endl;
-cin >> input;
-
-  cout << "Enter A for a integer, B or C for a double." << endl;
-  cin >> choice;
-
-if (choice == 'A')
-  cout << takeInt(input) << endl;
-else if ((choice == 'B') || (choice == 'C'))
-  cout << takeDouble(input) << endl;
-  }
+cout << endl;
   
+cout << "Abs Function with precision 2" << endl;
+cout << setprecision(2) << fixed << AbsFunt(num2) << endl << setprecision(4) << fixed;
 
+cout << endl;
+  
+cout << "Now testing variable mult, Enter num 4 and num 5: " << endl;
+cin >> num4 >> num5;
+mult (num4, num5);
 
+cout << endl;
+  
+cout << "Now testing variable SqrIt, enter num 6: "<< endl;
+cin >> num6;
+SqrIt (num6);
+  
+}
 
-
-//////////
+//Repeat Function
 void check (int num1, double num2, double num3)
 {
-
-  
+cout << "The value of integer 1 is " << num1 << endl;
+cout << "The value of double 2 is " << num2 << endl;
+cout << "The value of double 3 is " << num3 << endl;
 }
 
-//////////
-int takeInt(double x)
+// Absolute Function
+double AbsFunt (double x)
 {
-x = static_cast<int>(x);
-return x;
-}
-//////////
-double takeDouble(double x)
-{
-x = static_cast<double>(x);
-return x;
-  
+ if (x <= 0)
+  x = -x;
+return static_cast<double>(x);
 }
 
-////////////////
-/*
-double check (double x, char selection)
+// Multiply Function
+double mult (double num4, double num5)
 {
-char S;
-switch (S)
-  {
-    case (0):
-    case (1):
-      S = static_cast<int>(x);
-      break;
-    
-    case (2):
-      S = static_cast<double>(x);
-      break;
-    
-    case (3):
-      S = static_cast<double>(x);
-      break;
-    }
-return x;
-  } 
-*/
+double mult;
+cout << setprecision(4) << fixed;
+cout << "floating point of num 4 is " << num4 << endl;
+cout << "floating point of num 5 is " << num5 << endl;
+cout << endl;
+cout << "The multiplication of these two is " << num4 * num5 << endl;
+return mult;
+}
+
+//Square integer function
+int SqrIt (int num6)
+{
+int SqrIt;
+cout << "The int of num 6 is " << num6 << endl;
+cout << "The squared of the int is " << num6 * num6 << endl;
+return SqrIt;
+}
---
+++ .ccls-cache/@home@runner@CSCIHW6ZL/HW4@HW4.cpp
@@ -0,0 +1,8 @@
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
